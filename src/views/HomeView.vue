162587acbc7d9f66b225e76c340de217e918c8c5
ditorbejo <ditoooriq@gmail.com>
2022-06-30 18:30:11 +0700

    menambahkan padding pada image alur pendaftaran

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -371,6 +371,9 @@ main {
             aspect-ratio: 1/1;
             min-height: 400px;
             max-height: 800px;
+            background-color: #ffffff;
+            padding: 10px;
+            border-radius: 5px;
             box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
           }
         }
